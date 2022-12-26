0008b6b802554000d7cf32380ef6b1810af70a24
Nitesh Kumar <niteshman30@gmail.com>
2022-12-26 18:23:32 +0530

    add fab

--- app/src/main/java/com/example/school/AddProduct.kt
+++ app/src/main/java/com/example/school/AddProduct.kt
@@ -1,5 +1,6 @@
 package com.example.school
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class AddProduct : AppCompatActivity() {
     private lateinit var binding: ActivityAddProductBinding
-    lateinit var db:FirebaseFirestore
+    private lateinit var db:FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_product)
@@ -46,6 +47,8 @@ class AddProduct : AppCompatActivity() {
 
             fStore.document().set(model).addOnSuccessListener {
                 Toast.makeText(this@AddProduct, "data add successfuly", Toast.LENGTH_SHORT).show()
+                val intent= Intent(this@AddProduct,MainActivity::class.java)
+                startActivity(intent)
             }.addOnFailureListener {
                 Toast.makeText(this@AddProduct, "somethin went wrong", Toast.LENGTH_SHORT).show()
             }
