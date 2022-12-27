b41839bd80f6f8fc5c6978f5dd877e5c28624533
Nitesh Kumar <niteshman30@gmail.com>
2022-12-27 11:25:49 +0530

    add fab

--- app/src/main/java/com/example/school/MainActivity.kt
+++ app/src/main/java/com/example/school/MainActivity.kt
@@ -1,56 +1,92 @@
 package com.example.school
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.school.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var adapter: MyAdapter
-    lateinit var arrayList: ArrayList<Detail>
+    private lateinit var db: FirebaseFirestore
+    private lateinit var model:Detail
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MyAdapter
+    private lateinit var arrayList: ArrayList<Detail>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
-        arrayList= ArrayList()
-        arrayList.add(Detail(
-           R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Vipin","inactive"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Manish","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Rahul","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        arrayList.add(Detail(
-            R.drawable.student,"10th","9123471296","Nitesh","active"
-        ))
-        with(binding){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        buildRecycler()
+
+        with(binding) {
             fab.setOnClickListener {
-                val intent=Intent(this@MainActivity,AddProduct::class.java)
+                val intent = Intent(this@MainActivity, AddProduct::class.java)
                 startActivity(intent)
             }
-            adapter= MyAdapter(arrayList)
-            recyclerview.adapter=adapter
-            recyclerview.layoutManager=LinearLayoutManager(this@MainActivity)
+
         }
+    }
+
+    private fun buildRecycler() {
+        arrayList = ArrayList()
+         model = Detail()
+
+        db = FirebaseFirestore.getInstance()
+        val fStore = db.collection("student")
+
+        fStore.get().addOnSuccessListener {
+
+
+            if (!it.isEmpty) {
+
+                val list = it.documents
+
+                for (d in list) {
+                    model = d.toObject(Detail::class.java)!!
+                   arrayList.add(model)
+                    Log.d("dataaa",arrayList.toString())
+
+                    adapter = MyAdapter(arrayList)
+                    binding.recyclerview.adapter = adapter
+                    binding.recyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                }
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "data not found", Toast.LENGTH_SHORT).show()
+        }
+
+
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Vipin","inactive"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Manish","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Rahul","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+//        arrayList.add(Detail(
+//            R.drawable.student,"10th","9123471296","Nitesh","active"
+//        ))
+
     }
 }
